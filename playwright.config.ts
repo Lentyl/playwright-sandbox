7769e1ecb4e61b23ea18349698885eab6c35fdca
Mariusz Podgorski <m.b.podgorski1984@gmial.com>
2024-10-31 10:26:41 +0100

    docker-compose setup1.6

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'line' : 'html',
+  reporter: process.env.CI ? 'line' : 'line',
 
   use: {
     headless: true,
